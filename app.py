9b7e9fa11d4dc4e9b8e4281bcc31c758083cf209
kimbredenkamp <43311756+kimbredenkamp@users.noreply.github.com>
2019-06-04 09:36:46 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+
+@app.route('/database.html')
+def database():
+    return render_template('database.html')
+
+
+@app.route('/BLAST.html')
+def blast():
+    return render_template('top.html')
+
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
-    app.run()
-
-
+    app.run(debug=True)
