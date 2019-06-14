ea65d432be12d06cf11c784b4c72767146437e46
LarsMaas <43207345+LarsMaas@users.noreply.github.com>
2019-06-15 00:32:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,6 @@ def blast():
             text = "Dit is geen geldige sequentie of accessiecode"
         except ValueError:
             text = "Geef een dna of rna sequentie"
-    text = 'dit is een test fl;dsajlkfda;jlfda'
     return render_template('BLAST.html', text=text)
 
 
