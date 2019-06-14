8207a791c32af4dca02cc18cec80932be6e2f22c
kimbredenkamp <43311756+kimbredenkamp@users.noreply.github.com>
2019-06-14 16:02:08 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -8,9 +9,41 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/database.html')
+@app.route('/database.html', methods=['get'])
 def database():
-    return render_template('database.html')
+    connection = mysql.connector.connect(
+        host='hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com',
+        user='ophia@hannl-hlo-bioinformatica-mysqlsrv',
+        password='594990',
+        database='ophia')
+
+    definitions = ['Accessiecode', 'Bit_score', 'Query_coverage',
+                   'Description', 'E_value', 'Header', 'Perc_identity',
+                   'Eiwit_naam', 'Sequentie', 'Taxonomische_Rang']
+    filters = []
+
+    for defi in definitions:
+        if defi in request.args:
+            filters.append(defi)
+
+    if not filters:
+        filters = definitions
+    options = ','.join(filters)
+
+    query = "select " + options + " from eiwitten join resultaten r on " \
+                                  "eiwitten.Eiwit_id = r.Eiwit_id " \
+                                  "join input i " \
+                                  "on r.Sequentie_id = i.Sequentie_id join " \
+                                  "organismen o " \
+                                  "on r.Organisme_id = o.Organisme_id"
+    print(query)
+
+    cursor = connection.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return render_template('database.html', text=results)
 
 
 @app.route('/BLAST.html')
