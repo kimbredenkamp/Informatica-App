22ff8ddfb2e6d903d0af990f6a96274af3bd0bae
LarsMaas <43207345+LarsMaas@users.noreply.github.com>
2019-06-14 23:10:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,38 +11,39 @@ def home():
 
 @app.route('/database.html', methods=['get'])
 def database():
-    connection = mysql.connector.connect(
-        host='hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com',
-        user='ophia@hannl-hlo-bioinformatica-mysqlsrv',
-        password='594990',
-        database='ophia')
+    results = 'hi'
+#     connection = mysql.connector.connect(
+#         host='hannl-hlo-bioinformatica-mysqlsrv.mysql.database.azure.com',
+#         user='ophia@hannl-hlo-bioinformatica-mysqlsrv',
+#         password='594990',
+#         database='ophia')
 
-    definitions = ['Accessiecode', 'Bit_score', 'Query_coverage',
-                   'Description', 'E_value', 'Header', 'Perc_identity',
-                   'Eiwit_naam', 'Sequentie', 'Taxonomische_Rang']
-    filters = []
+#     definitions = ['Accessiecode', 'Bit_score', 'Query_coverage',
+#                    'Description', 'E_value', 'Header', 'Perc_identity',
+#                    'Eiwit_naam', 'Sequentie', 'Taxonomische_Rang']
+#     filters = []
 
-    for defi in definitions:
-        if defi in request.args:
-            filters.append(defi)
+#     for defi in definitions:
+#         if defi in request.args:
+#             filters.append(defi)
 
-    if not filters:
-        filters = definitions
-    options = ','.join(filters)
+#     if not filters:
+#         filters = definitions
+#     options = ','.join(filters)
 
-    query = "select " + options + " from eiwitten join resultaten r on " \
-                                  "eiwitten.Eiwit_id = r.Eiwit_id " \
-                                  "join input i " \
-                                  "on r.Sequentie_id = i.Sequentie_id join " \
-                                  "organismen o " \
-                                  "on r.Organisme_id = o.Organisme_id"
-    print(query)
+#     query = "select " + options + " from eiwitten join resultaten r on " \
+#                                   "eiwitten.Eiwit_id = r.Eiwit_id " \
+#                                   "join input i " \
+#                                   "on r.Sequentie_id = i.Sequentie_id join " \
+#                                   "organismen o " \
+#                                   "on r.Organisme_id = o.Organisme_id"
+#     print(query)
 
-    cursor = connection.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    cursor.close()
-    connection.close()
+#     cursor = connection.cursor()
+#     cursor.execute(query)
+#     results = cursor.fetchall()
+#     cursor.close()
+#     connection.close()
     return render_template('database.html', text=results)
 
 
